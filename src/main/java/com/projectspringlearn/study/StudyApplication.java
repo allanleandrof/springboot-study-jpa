38867eb404013e700b9140787bfae787c7a4a21a
Allan Leandro <alanleandrof@gmail.com>
2024-07-20 12:31:07 -0300

    project created

---
+++ src/main/java/com/projectspringlearn/study/StudyApplication.java
@@ -0,0 +1,14 @@
+package com.projectspringlearn.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+		System.out.println("Hello, World!");
+	}
+
+}
