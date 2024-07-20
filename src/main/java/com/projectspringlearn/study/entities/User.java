b79a9488842148fa67a25988c6e8945747706ea4
Allan Leandro <alanleandrof@gmail.com>
2024-07-20 17:31:52 -0300

    H2 DB, test profile, jpa

--- src/main/java/com/projectspringlearn/study/entities/User.java
+++ src/main/java/com/projectspringlearn/study/entities/User.java
@@ -1,9 +1,20 @@
 package com.projectspringlearn.study.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
